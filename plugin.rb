bbbc70aeebbe1f3e3c4f5d8ea401dd51c5ed0fdb
Julius Ijidola <65051450+juliusijidola@users.noreply.github.com>
2026-04-08 11:34:31 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,29 +1,32 @@
-# frozen_string_literal: true
-
-# name: krabit-escrow
-# about: Simple escrow system for KRABIT
+# name: discourse-escrow
+# about: Buyer/Seller escrow protection with NGN, USDT, USDC support
 # version: 1.0.0
-# authors: KRABIT
+# authors: Techhive
+# url: https://github.com/Techhive-Technologies/escrow/
 
-enabled_site_setting :krabit_escrow_enabled
+enabled_site_setting :escrow_enabled
+
+register_asset 'stylesheets/escrow.scss'
 
 after_initialize do
-  require_relative "app/models/krabit_escrow"
-  require_relative "app/serializers/krabit_escrow_serializer"
-  require_relative "app/controllers/krabit/escrows_controller"
+  require_dependency 'application_controller'
+
+  [
+    '../app/models/escrow_transaction',
+    '../app/controllers/discourse_escrow/escrow_controller',
+    '../app/serializers/escrow_transaction_serializer'
+  ].each { |path| load File.expand_path(path + '.rb', __FILE__) }
 
   Discourse::Application.routes.append do
-    namespace :krabit do
-      resources :escrows, only: %i[index show create] do
-        member do
-          post :accept
-          post :decline
-          post :mark_delivered
-          post :confirm
-          post :dispute
-          post :cancel
-        end
-      end
+    scope '/escrow' do
+      get  '/'              => 'discourse_escrow/escrow#index'
+      post '/create'        => 'discourse_escrow/escrow#create'
+      post '/:id/fund'      => 'discourse_escrow/escrow#fund'
+      post '/:id/release'   => 'discourse_escrow/escrow#release'
+      post '/:id/dispute'   => 'discourse_escrow/escrow#dispute'
+      get  '/:id'           => 'discourse_escrow/escrow#show'
+      post '/webhook/paystack'     => 'discourse_escrow/escrow#paystack_webhook'
+      post '/webhook/nowpayments'  => 'discourse_escrow/escrow#nowpayments_webhook'
     end
   end
 end
