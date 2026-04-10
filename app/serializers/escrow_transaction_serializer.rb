c97d6f48a224cde7fbc8426bd908e532ed7d26c5
Julius Ijidola <65051450+juliusijidola@users.noreply.github.com>
2026-04-10 22:45:44 +0100

    Update escrow_transaction_serializer.rb

--- app/serializers/escrow_transaction_serializer.rb
+++ app/serializers/escrow_transaction_serializer.rb
@@ -1,5 +1,6 @@
 class EscrowTransactionSerializer < ApplicationSerializer
   attributes :id,
+             :title,
              :buyer_username,
              :buyer_avatar,
              :seller_username,
@@ -11,7 +12,9 @@ class EscrowTransactionSerializer < ApplicationSerializer
              :payment_network,
              :status,
              :description,
+             :dispute_reason,
              :payment_address,
+             :pm_url,          # ← new
              :created_at,
              :funded_at,
              :released_at,
@@ -19,31 +22,26 @@ class EscrowTransactionSerializer < ApplicationSerializer
              :is_buyer,
              :is_seller
 
-  def buyer_username
-    object.buyer.username
+  def title
+    object.title.to_s
   end
 
-  def buyer_avatar
-    object.buyer.avatar_template
-  end
+  def buyer_username;  object.buyer.username;        end
+  def buyer_avatar;    object.buyer.avatar_template;  end
+  def seller_username; object.seller.username;        end
+  def seller_avatar;   object.seller.avatar_template; end
+  def total_with_fee;  object.total_with_fee;         end
 
-  def seller_username
-    object.seller.username
-  end
+  def is_buyer;  scope&.current_user&.id == object.buyer_id;  end
+  def is_seller; scope&.current_user&.id == object.seller_id; end
 
-  def seller_avatar
-    object.seller.avatar_template
-  end
+  # Find the PM topic that was created for this deal
+  def pm_url
+    topic = Topic.where(
+      archetype: Archetype.private_message,
+      user_id:   object.buyer_id
+    ).where("title LIKE ?", "🛡️ Escrow ##{object.id}%").first
 
-  def total_with_fee
-    object.total_with_fee
-  end
-
-  def is_buyer
-    scope&.current_user&.id == object.buyer_id
-  end
-
-  def is_seller
-    scope&.current_user&.id == object.seller_id
+    topic ? "/t/#{topic.slug}/#{topic.id}" : nil
   end
 end
