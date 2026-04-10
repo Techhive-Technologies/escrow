c7dbde68d8a63ef169b579f3b0e6c137e5b6f01c
Julius Ijidola <65051450+juliusijidola@users.noreply.github.com>
2026-04-10 12:30:57 +0100

    Update escrow-init.js

--- assets/javascripts/discourse/initializers/escrow-init.js
+++ assets/javascripts/discourse/initializers/escrow-init.js
@@ -3,23 +3,16 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 export default {
   name: "escrow-init",
   initialize() {
-    withPluginApi("0.8.31", (api) => {
+    withPluginApi("2.0.0", (api) => {
 
-      // Add "Escrow" to the top navigation menu
-      api.decorateWidget("header-icons:before", (helper) => {
-        return helper.h(
-          "li.header-escrow-icon",
-          helper.h(
-            "a",
-            {
-              href: "/escrow-page",
-              title: "Escrow",
-              className: "icon btn-flat",
-            },
-            helper.h("span", { className: "d-icon d-icon-shield-alt" }, "🛡")
-          )
-        );
+      // Add "Escrow" link to the sidebar or top nav
+      api.addNavigationBarItem({
+        name: "escrow",
+        displayName: "🛡️ Escrow",
+        href: "/my-escrows",
+        title: "Escrow Deals",
       });
+
     });
   },
 };
