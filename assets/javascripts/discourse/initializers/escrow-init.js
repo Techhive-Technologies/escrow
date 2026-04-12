09becf46a60498753c55c39f4104eb447e699c01
Julius Ijidola <65051450+juliusijidola@users.noreply.github.com>
2026-04-12 05:13:46 +0100

    Update escrow-init.js

--- assets/javascripts/discourse/initializers/escrow-init.js
+++ assets/javascripts/discourse/initializers/escrow-init.js
@@ -1,8 +1,23 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import MyEscrowsRoute from "../routes/my-escrows";
+import MyEscrowsController from "../controllers/my-escrows";
 
 export default {
   name: "escrow-init",
-  initialize() {
-    withPluginApi("0.8.31", () => {});
+  initialize(container) {
+    withPluginApi("2.0.0", (api) => {
+
+      api.addNavigationBarItem({
+        name:        "escrow",
+        displayName: "🛡️ Escrow",
+        href:        "/my-escrows",
+        title:       "My Escrow Deals",
+      });
+
+    });
+
+    // This is what actually makes /my-escrows work in Ember
+    container.register("route:my-escrows",      MyEscrowsRoute);
+    container.register("controller:my-escrows", MyEscrowsController);
   },
 };
